b89389f65e1772f5bdd26c38159169756c68ae3c
qq1064050618 <1064050618@qq.com>
2019-07-20 10:22:41 +0800

    first try

---
+++ Try.java
@@ -0,0 +1,30 @@
+package com.neuedu.objcet;
+
+public class Try {
+
+  private int id;
+  private String name;
+    public Try(int id,String name){
+this.id=id;
+this.name=name;
+    }
+    public void  sout(){
+        System.out.println("id+"+id);
+        System.out.println("name+"+name);
+    }
+   /* public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }*/
+}
