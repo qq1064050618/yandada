15a0fe4a668829d838839d4d1fb2b0b9ce0be64e
qq1064050618 <1064050618@qq.com>
2019-07-20 10:29:46 +0800

    third try

--- Try.java
+++ Try.java
@@ -19,12 +19,5 @@ this.name=name;
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }*/
+*/
 }
